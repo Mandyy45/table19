32d8bd30ffc8c04e6b4d9a010aa884e8c0a6cf78
Manideep Devarashetty <44manideep@gmail.com>
2023-04-16 15:59:17 -0500

    added table19 with thop

---
+++ src/main/java/table19/table_19.java
@@ -0,0 +1,9 @@
+package table19;
+
+public class table_19 {
+    public static void main(String[] args) {
+        int num = 19;
+        for (int i = 0; i <= 10; i++)
+            System.out.println(num + "*" + i + "=" + num * i);
+    }
+}
